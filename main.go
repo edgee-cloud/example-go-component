765c38d287f9774a2205a3779b869f06b9bc6d65
Alex Casalboni <alex@edgee.cloud>
2025-02-21 14:48:38 +0100

    rename cred to settings in main.go

--- main.go
+++ main.go
@@ -16,14 +16,14 @@ func resultWrapper(request dc.EdgeeRequest) (result Result) {
 }
 
 func init() {
-	dc.Exports.Page = func(e dc.Event, cred dc.Dict) Result {
-		return resultWrapper(PageHandler(e, cred))
+	dc.Exports.Page = func(e dc.Event, settings dc.Dict) Result {
+		return resultWrapper(PageHandler(e, settings))
 	}
-	dc.Exports.Track = func(e dc.Event, cred dc.Dict) Result {
-		return resultWrapper(TrackHandler(e, cred))
+	dc.Exports.Track = func(e dc.Event, settings dc.Dict) Result {
+		return resultWrapper(TrackHandler(e, settings))
 	}
-	dc.Exports.User = func(e dc.Event, cred dc.Dict) Result {
-		return resultWrapper(UserHandler(e, cred))
+	dc.Exports.User = func(e dc.Event, settings dc.Dict) Result {
+		return resultWrapper(UserHandler(e, settings))
 	}
 }
 
